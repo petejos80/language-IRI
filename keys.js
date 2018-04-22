4ee87da4b6cb883488bc06854c8319f4c6241be3
petejos80 <petejos80@gmail.com>
2018-04-22 13:52:16 -0500

    Added and removed API keys

--- keys.js
+++ keys.js
@@ -1,14 +1,13 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.Pc0hyiuLiq49qzjrIpaykYncy,
-  consumer_secret: process.env.VrM1iYav0oZQhesI5za6hEXgmU5iAzPWIoN6wtub9oGzRnyyUF,
-  access_token_key: process.env.986073590306148352-0Z5x5d0AkG9RoAsRu676YngzK2yUHtJ,
-  access_token_secret: process.env.Zfvpevv6S15ff1q5tSWWiPGx8hRCR8f0ykZIytFcXgLFa
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
-
